2235e27235e1ec7f6da218e363022d141074784b
Christian L <Lehmanc92@gmail.com>
2021-07-13 00:38:13 -0400

    demo info

--- Code/static/js/app.js
+++ Code/static/js/app.js
@@ -17,12 +17,42 @@ d3.json(url).then(function(data) {
         dropDown.append("option").text(sample).property("value", sample);
     });
 
-    // Grabbing corresponding OTU values from top 10 Sample values
+    // Defining variable for the current subject selected
 
-    var top10OTU = data.otu_ids.slice(0, 10);
-    console.log(top10OTU);
-    
+    var currentSubject = dropDown.property("value");
+    console.log(currentSubject);
 
+    var samples = data.samples;
+    var metadata = data.metadata;
+
+    // Grabbing corresponding OTU values from top 10 Sample values 
+
+    // Creating empty variables for forEach loop
+    var currentID;
+    var currentMetadata;
+    var currentNames;
+
+    samples.forEach( sample => {
+        if (sample.id == currentSubject) {
+            currentID = sample.id
+        }
+        // console.log(currentID)
+    });
+
+    metadata.forEach( metadatum => {
+        if (metadatum.id == currentSubject) {
+            currentMetadata = metadatum           
+        }
+        // console.log(currentMetadata)
+    });
+
+    // Dynamically updating HTML
+
+    d3.select("#sample-metadata").html("");
+
+    for (item in currentMetadata) {
+        d3.select("#sample-metadata").append("ul").text(item + ": " + currentMetadata[item]);
+    }
 
 
 });
